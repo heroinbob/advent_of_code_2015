3a0b88b710ab4a35ba347d25e8d5d5c75a91166a
heroinbob <mackenzie.luceononuro@gmail.com>
2015-12-03 15:18:42 -0800

    solved part 2 of day 3 in elixir

--- elixir/lib/advent/day3.ex
+++ elixir/lib/advent/day3.ex
@@ -32,8 +32,9 @@ defmodule Advent.Day3 do
   """
 
   def unique_houses(commands) do
-    map = mark_spot(HashDict.new, 0, 0)
-    move(commands, 0, 0, map)
+    commands
+    |> visits
+    |> length
   end
 
   def move(">" <> rest, x, y, map) do
@@ -61,11 +62,65 @@ defmodule Advent.Day3 do
   end
 
   def move("", _x, _y, map) do
-    Enum.count(Dict.keys(map))
+    Dict.keys(map)
   end
 
   def mark_spot(map, x, y) do
     key = "#{x},#{y}"
     Dict.put(map, key, 1)
+  end
+
+  def visits(commands) do
+    map = mark_spot(HashDict.new, 0, 0)
+    move(commands, 0, 0, map)
+  end
+
+  @doc """
+  --- Part Two ---
+
+  The next year, to speed up the process, Santa creates a robot version of
+  himself, Robo-Santa, to deliver presents with him.
+
+  Santa and Robo-Santa start at the same location (delivering two presents to
+  the same starting house), then take turns moving based on instructions from
+  the elf, who is eggnoggedly reading from the same script as the previous year.
+
+  This year, how many houses receive at least one present?
+
+  For example:
+
+  ^v delivers presents to 3 houses, because Santa goes north, and then
+  Robo-Santa goes south.
+
+  ^>v< now delivers presents to 3 houses, and Santa and Robo-Santa end up back
+  where they started.
+
+  ^v^v^v^v^v now delivers presents to 11 houses, with Santa going one direction
+  and Robo-Santa going the other.
+
+  Correct answer is 2361 for my data set.
+  """
+  def take_turns(commands) do
+    santa_visits(commands)
+    |> Enum.concat(robot_visits(commands))
+    |> Enum.uniq
+    |> length
+  end
+
+  def santa_visits(commands) do
+    isolate(commands, 0) |> visits
+  end
+
+  def robot_visits(commands) do
+    isolate(commands, 1) |> visits
+  end
+
+  def isolate(commands, start_index) do
+    size = String.length(commands)
+
+    String.slice(commands, start_index..size)
+    |> String.split("")
+    |> Enum.take_every(2)
+    |> Enum.join("")
   end
 end
