84478f0145a7aff100f4141d522c09498d3b8d34
heroinbob <mackenzie.luceononuro@gmail.com>
2015-12-08 22:25:41 -0800

    solved Day6 part 1 in elixir

--- elixir/lib/advent/day6.ex
+++ elixir/lib/advent/day6.ex
@@ -6,84 +6,48 @@ defmodule Advent.Day6 do
     File.read!("data/day6.txt")
     |> String.split("\n")
     |> Enum.reject(fn(v) -> v == "" end)
-    |> Enum.take(10)
+    |> Command.parse
   end
 
   def light! do
-    instructions
-    |> Command.parse
-    |> do_commands(Grid.new(1_000))
+    performer = fn(instruction, lights) -> perform(instruction, lights) end
+    Enum.reduce(instructions, Grid.new, performer)
   end
 
-  @doc """
-  Returns a grid updated by the actions.
-    iex > Advent.Day6.do_commands(
-    ...>   [
-    ...>    {"on", {1..2, 1..2}},
-    ...>    {"off", {3..4, 3..4}},
-    ...>    {"toggle", {3..4, 3..4}}
-    ...>   ],
-    ...>   Grid.new(2)
-    ...> )
-    []
-  """
-  def do_commands([command | tail], grid) do
-    new_grid = perform(command, grid)
-    do_commands(tail, new_grid)
+  def lit_count do
+    light! |> Enum.count(fn{_, status} -> status end)
   end
 
-  def do_commands([], grid) do
-    grid
+  def perform({"on", ranges}, grid) do
+    on(grid, mix(ranges))
   end
 
-  @doc """
-  Returns the updated grid with x, y turned on.
-
-    iex> Advent.Day6.perform({"on", {0..1, 0..1}}, Advent.Day6.Grid.new(2))
-    [[true, true], [true, true]]
-  """
-  def perform({"on", {x_range, y_range}}, grid) do
-    on(grid, mix(x_range, y_range))
+  def perform({"off", ranges}, grid) do
+    off(grid, mix(ranges))
   end
 
-  @doc """
-  Returns the updated grid with lights turned off.
-
-    iex> Advent.Day6.perform({"off", {0..1, 0..1}}, Advent.Day6.Grid.new(2, true))
-    [[false, false], [false, false]]
-  """
-  def perform({"off", {x_range, y_range}}, grid) do
-    off(grid, mix(x_range, y_range))
+  def perform({"toggle", ranges}, grid) do
+    toggle(grid, mix(ranges))
   end
 
-  @doc """
-  Returns the updated grid with x, y toggled.
-
-    iex> Advent.Day6.perform({"toggle", {0..1, 0..1}}, Advent.Day6.Grid.new(2))
-    [[true, false], [true, false]]
-  """
-  def perform({"toggle", {x_range, y_range}}, grid) do
-    toggle(grid, mix(x_range, y_range))
-  end
-
-  def on(grid, [coords | tail]) do
-    Grid.update(grid, coords, true) |> on(tail)
+  def on(grid, [coord | tail]) do
+    Grid.update(grid, coord, true) |> on(tail)
   end
 
   def on(grid, []) do
     grid
   end
 
-  def off(grid, [coords | tail]) do
-    Grid.update(grid, coords, false) |> off(tail)
+  def off(grid, [coord | tail]) do
+    Grid.delete(grid, coord) |> off(tail)
   end
 
   def off(grid, []) do
     grid
   end
 
-  def toggle(grid, [coords | tail]) do
-    Grid.toggle(grid, coords) |> toggle(tail)
+  def toggle(grid, [coord | tail]) do
+    Grid.toggle(grid, coord) |> toggle(tail)
   end
 
   def toggle(grid, []) do
@@ -96,7 +60,7 @@ defmodule Advent.Day6 do
     iex> Advent.Day6.mix(1..3, 4..6)
     [{1, 4}, {1, 5}, {1, 6}, {2, 4}, {2, 5}, {2, 6}, {3, 4}, {3, 5}, {3, 6}]
   """
-  def mix(range1, range2) do
+  def mix({range1, range2}) do
     Enum.flat_map(range1, fn(r1) ->
       Enum.map(range2, fn(r2) -> {r1, r2} end)
     end)
--- elixir/lib/advent/day_6/grid.ex
+++ elixir/lib/advent/day_6/grid.ex
@@ -1,23 +1,23 @@
 defmodule Advent.Day6.Grid do
-  def new(size, value \\ false) do
-    Enum.to_list(1..size) |> Enum.map(fn(_) -> new_row(size, value) end)
+  def new do
+    %{}
   end
 
-  def new_row(size, value) do
-    Enum.to_list(1..size) |> Enum.map(fn(_) -> value end)
+  def update(grid, coord, value) do
+    Dict.put(grid, key(coord), value)
   end
 
-  def value(grid, {x, y}) do
-    Enum.at(grid, y) |> Enum.at(x)
-  end
-
-  def update(grid, {x, y}, value) do
-    row = Enum.at(grid, y)
-    updated_row = List.replace_at(row, x, value)
-    List.replace_at(grid, y, updated_row)
+  def delete(grid, coord) do
+    Dict.delete(grid, key(coord))
   end
 
   def toggle(grid, coord) do
-    update(grid, coord, !value(grid, coord))
+    Dict.get_and_update(grid, key(coord), fn(value) -> {value, !value} end)
+    |> Tuple.to_list
+    |> List.last
+  end
+
+  def key({x, y}) do
+    "#{x},#{y}"
   end
 end
